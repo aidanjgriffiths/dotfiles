ea9612e41f5264d7ed3f167d9adfb1a3c12bca31
Aidan Griffiths <aidanjames.g@gmail.com>
2024-01-29 11:22:44 +1100

    chore: Update default lsp and linters with MasonInstall

--- nvim/after/plugin/lint.lua
+++ nvim/after/plugin/lint.lua
@@ -1,14 +1,17 @@
 require("lint").linters_by_ft = {
-	python = { "pylint", "ruff" },
-	markdown = { "markdownlint" },
-	lua = { "luacheck" },
-	yaml = { "ansible-lint" },
+    sh = { "shellcheck" },
+    python = { "ruff" },
+    markdown = { "markdownlint" },
+    lua = { "luacheck" },
+    go = { "glint" },
+    terraform = { "tflint" },
+    yaml = { "ansible-lint" },
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-	callback = function()
-		require("lint").try_lint()
-	end,
+    callback = function()
+        require("lint").try_lint()
+    end,
 })
 
 vim.keymap.set("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>")
